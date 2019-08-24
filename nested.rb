6c069b66e9fbdfd430450ae965d7c5f99b685aeb
John Guest <jcguest90@gmail.com>
2019-08-24 17:28:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    programmer_hash[:yukihiro_matsumoto] 
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 end
 
 def changing_alan
